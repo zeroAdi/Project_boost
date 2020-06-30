f763d12c64047d6c06d8c0b249d598d15ff6cef8
zeroAdi <adityagond015@gmail.com>
2020-06-30 14:21:14 +0530

    rocket movement

---
+++ Project_Boost/Assets/rocket.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocket : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        processInput();
+    }
+    private void processInput(){
+        if (Input.GetKey(KeyCode.Space)){
+            rigidbody.AddRelativeForce(Vector3.up);
+        }
+         if (Input.GetKey(KeyCode.A)){
+            transform.Rotate(-Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D)){
+            transform.Rotate(-Vector3.forward);
+        }
+
+    }
+}

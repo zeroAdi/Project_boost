e159bc0276b2f181cf3f6cfe897a95f7a2935432
zeroAdi <adityagond015@gmail.com>
2020-06-30 20:34:10 +0530

    sound and movement
    added sound and fixed pivot and center point.

--- Project_Boost/Assets/rocket.cs
+++ Project_Boost/Assets/rocket.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class rocket : MonoBehaviour
 {
     Rigidbody rigidbody;
-    // Start is called before the first frame update
+    AudioSource audioSource;    // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -18,10 +19,17 @@ public class rocket : MonoBehaviour
     }
     private void processInput(){
         if (Input.GetKey(KeyCode.Space)){
+            if(!audioSource.isPlaying){
+                 audioSource.Play();
+            }
+            else{
+                audioSource.Stop();
+            }
+           
             rigidbody.AddRelativeForce(Vector3.up);
         }
          if (Input.GetKey(KeyCode.A)){
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D)){
             transform.Rotate(-Vector3.forward);
